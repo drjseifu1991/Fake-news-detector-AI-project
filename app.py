b24e0382a4a188f1bbb9fd17bcd6c882102a07d3
Dereje3030 <51210918+Dereje3030@users.noreply.github.com>
2022-07-08 10:34:36 +0300

    After finishing the project

---
+++ app.py
@@ -0,0 +1,36 @@
+from flask import Flask, render_template, request, jsonify
+import nltk
+import pickle
+from nltk.corpus import stopwords
+import re
+from nltk.stem.porter import PorterStemmer
+app = Flask(__name__)
+ps = PorterStemmer()
+# Load model and vectorizer
+model = pickle.load(open('fake_news_model2.pkl', 'rb'))
+tfidfvect = pickle.load(open('tfidfvec.pkl', 'rb'))
+# Build functionalities
+@app.route('/', methods=['GET'])
+def home():
+    return render_template('index.html')
+def predict(text):
+    review = re.sub('[^a-zA-Z]', ' ', text)
+    review = review.lower()
+    review = review.split()
+    review = [ps.stem(word) for word in review if not word in stopwords.words('english')]
+    review = ' '.join(review)
+    review_vect = tfidfvect.transform([review]).toarray()
+    prediction = 'FAKE' if model.predict(review_vect) == 0 else 'REAL'
+    return prediction
+@app.route('/', methods=['POST'])
+def webapp():
+    text = request.form['text']
+    prediction = predict(text)
+    return render_template('index.html', text=text, result=prediction)
+@app.route('/predict/', methods=['GET','POST'])
+def api():
+    text = request.args.get("text")
+    prediction = predict(text)
+    return jsonify(prediction=prediction)
+if __name__ == "__main__":
+    app.run()
--- env/Library/lib/sqlite3.34.0/pkgIndex.tcl
+++
@@ -1,1 +0,0 @@
-package ifneeded sqlite3 3.34.0 [list load [file join $dir sqlite3340t.dll] sqlite3] 
